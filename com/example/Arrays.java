75bc177922bd4e0946901ac6ef44f2197c9cfdc9
PencersGitHub <113054133+PencersGitHub@users.noreply.github.com>
2024-04-04 10:25:14 -0400

    Initial commit

---
+++ com/example/Arrays.java
@@ -0,0 +1,28 @@
+
+package com.example;
+
+/**
+ * Change me.
+ *
+ * @author Spencer Scarlett
+ * @version 1.0
+ * @since 2024-04-04
+ */
+
+// Arrays class
+public final class Arrays {
+
+    /** Private constructor to prevent instantiation. */
+    private Arrays() {
+        throw new UnsupportedOperationException("Cannot instantiate");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world");
+    }
+}
